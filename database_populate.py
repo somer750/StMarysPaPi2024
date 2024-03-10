119db5ef354ca754bd35a5033f75dd1350b58073
somer750 <62039275+somer750@users.noreply.github.com>
2024-03-10 22:55:55 +0000

    Update database_populate.py

--- database_populate.py
+++ database_populate.py
@@ -1,35 +1,58 @@
+# you MUST use sudo privileges to run this program
+# working version,reduced brightness in leds
+
 import mariadb
-import random
+import board
+import neopixel
 import time
+import random
+pixels = neopixel.NeoPixel(board.D18, 30)
 
-pupils = ["Alex", "Cameron", "Charlie", "Chris", "Daniel", "David", "Erin", "Isabel",
- "James", "Jessica", "John", "Joshua", "Lexi", "Lois", "Lucy", "Mary", "Max",
- "Michael", "Mimi", "Nathaniel", "Nicholas", "Olivia", "Paul", "Phoebe", "Rachel",
- "Ryan", "Sally", "Samantha", "Seb", "Susan" ]
-
+# connect to the database
 connection = mariadb.connect(
     user="stmarys",
     password="5ch00l##",
     host="192.168.2.150",
     database="StMarysSchool_MariaDB")
 
+pupils = ["Alex", "Cameron", "Charlie", "Chris", "Daniel", "David", "Erin", "Isabel",
+ "James", "Jessica", "John", "Joshua", "Lexi", "Lois", "Lucy", "Mary", "Max",
+ "Michael", "Mimi", "Nathaniel", "Nicholas", "Olivia", "Paul", "Phoebe", "Rachel",
+ "Ryan", "Sally", "Samantha", "Seb", "Susan" ]
+
 #A cursor is a database object that retrieves and also updates data, one row at a time, from a set of data.
 cursor = connection.cursor()
 
-def add_data(device, student, status):
+def get_data(studentquery):
     try:
-        statement = "INSERT INTO students (device, student, status) VALUES (%s, %s, %d)"
-        data = (device, student, status)
-        cursor.execute(statement, data)
-        connection.commit()
-        print("Successfully added entry to database")
-    except database.Error as e:
-        print(f"Error adding entry to database: {e}")
+      statement = "SELECT student, status FROM students WHERE student=%s ORDER BY record_id DESC"
+      data = (studentquery,)
+      cursor.execute(statement, data)
+      row = cursor.fetchone()
+      if row is not None: # this will only pull the very first result
+        return(row[1])     # this returns the second part of the result, expressed as an integer
+      else:
+        print("no entry found")
+    except mariadb.Error as e:
+      print(f"Error retrieving entry from database: {e}")
 
-for pupil in pupils:
-    stations = ["pupil_station_1", "pupil_station_2", "pupil_station_3", "pupil_station_4", "pupil_station_5", "water_fountain", "playground", "panic_button", "pocket_module"]
-    status = random.randint(1,5)
-    station = random.choice(stations)
-    add_data(station, pupil, status)
+# clear the display
+for x in range(0, 30):
+    pixels[x] = (0, 0, 0)
+print("Display cleared. Press CTRL + C to stop the program")
 
+# wait a short while to allow the user to cancel the program (in case they wanted to clear the screen)
+for x in range (1,6):
+    print("Program will continue in ",6-x," seconds")
+    time.sleep(1)
+
+while True:
+    for pupilfetch in pupils:
+        pupil = get_data(pupilfetch)
+        print(pupilfetch,pupil)
+    break
+
+cursor.close()
 connection.close()
+
+
